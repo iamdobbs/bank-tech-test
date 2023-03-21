18d073b219da82adc27d6fc1a89d619e7524a3c4
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-21 16:48:58 +0000

    Create Transaction class. Set attributes, tests pass

---
+++ lib/transaction.rb
@@ -0,0 +1,11 @@
+class Transaction
+  attr_reader :date, :amount, :transaction_type, :balance
+
+  def initialize(date, amount, transaction_type, balance)
+    @date = date
+    @amount = amount
+    @transaction_type = transaction_type
+    @balance = balance
+  end  
+
+end  
---
+++ spec/unit/transaction_spec.rb
@@ -0,0 +1,19 @@
+require_relative '../../lib/transaction'
+
+describe Transaction do
+  let(:date) { '01/01/2023' }
+  let(:amount) { 1000 }
+  let(:transaction_type) { 'credit' }
+  let(:balance) { 2000 }
+
+  subject(:transaction) { described_class.new(date, amount, transaction_type, balance) }
+
+  context 'when Transaction class is initialized' do
+    it 'sets the relevant attribute' do
+      expect(transaction.date).to eq(date)
+      expect(transaction.amount).to eq(amount)
+      expect(transaction.transaction_type).to eq(transaction_type)
+      expect(transaction.balance).to eq(balance)
+    end
+  end
+end  
