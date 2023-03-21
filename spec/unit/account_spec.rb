cb361a43a33c72c331185bccc6e02c93a3e12163
Hayley Dobbs <iamdobbs@icloud.com>
2023-03-21 15:53:30 +0000

    Rename test context to initialization

--- spec/unit/account_spec.rb
+++ spec/unit/account_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../../lib/bank_account'
 
 describe BankAccount do
-  context 'bank account unit tests' do
+  context 'bank account initialization tests' do
     it 'initializes with a balance of zero' do
       account = BankAccount.new
       expect(account.balance).to eq(0)
